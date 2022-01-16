9d40b63dc9fe1b8a587878bc36d3f4b84395b836
NextinMono <lorenzorai05@gmail.com>
2022-01-16 10:52:30 +0100

    goddddddddddddddddddddd

---
+++ Runtime/Spettro/Discord/DiscordController.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Discord;
+using System;
+using Spettro.DiscordGameSDK;
+
+namespace Spettro.DiscordGameSDK
+{
+    public class DiscordController : MonoBehaviour
+    {
+        public bool setDDOLAutomatically = true;
+        public DiscordActivityObject[] activities;
+        public Int64 appID;
+        public Discord.Discord discord;
+        public string defaultBigImageKey;
+        
+        private ActivityManager activityManager;
+        public Activity activity;
+
+        // Use this for initialization
+        public static DiscordController discordInstance { get; private set; }
+
+        void Awake()
+        {
+            if (setDDOLAutomatically)
+                DontDestroyOnLoad(this);
+
+            if (discordInstance == null)
+            {
+                discordInstance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+        void Start()
+        {
+            //NoRequireDiscord : Doesn't need discord to run the game
+            //Default : does require discord to run the game, and will try to open it.
+            discord = new Discord.Discord(appID, (ulong)Discord.CreateFlags.NoRequireDiscord);
+
+            //Check the scene name and set it accordingly
+            SceneManager.sceneLoaded += RunMethodCheck;
+
+            //Get activity manager
+            activityManager = discord.GetActivityManager();
+            //Make a temporary activity, more likely than not this'll never show.
+            activity = new Discord.Activity()
+            {
+                State = "Discord State not set yet!"
+            };
+            //Do the same check it would do when a scene changes but when the current one loads
+            CheckLevelName(SceneManager.GetActiveScene());
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            try
+            {
+                discord.RunCallbacks();
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"DiscordManager lost callbacks!\n{ex}", this);
+                this.enabled = false;
+            }
+
+        }
+
+        void OnApplicationQuit()
+        {
+            //Dispose of the rich presence
+            try
+            {
+                discord.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"DiscordManager was unable to dispose of Discord Activity!\n{ex}", this);
+            }
+        }
+        
+        void RunMethodCheck(Scene scene, LoadSceneMode mode)
+        {
+            CheckLevelName(scene);
+        }
+        void CheckLevelName(Scene scene)
+        {
+            ///Grab the scene name
+            string lvlName = scene.name;
+            int count = 0;
+            long timestamp = 0;
+            ///Foreach activity in the activities array, check if any activity's scene value is equal to the scene name
+            ///If yes, apply it.
+            ///If no, continue until it finds one or doesn't.
+            foreach (DiscordActivityObject activityobject in activities)
+            {
+                if (activityobject.scene == lvlName)
+                {
+                    count += 1;
+                    ///If the big image key is null, set it to the default big icon (instead of leaving it empty)
+                    if (string.IsNullOrEmpty(activityobject.bigImageKey))
+                    {
+                        activityobject.bigImageKey = defaultBigImageKey;
+                    }
+                    switch(activityobject.timestampMode)
+                    {
+                        case DiscordActivityObject.TimeStampMode.None:
+                            {
+                                timestamp = 0;
+                                break;
+                            }
+                        case DiscordActivityObject.TimeStampMode.Elapsed:
+                            {
+                                timestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
+                                break;
+                            }
+                        case DiscordActivityObject.TimeStampMode.Remaining:
+                            {
+                                Debug.Log("Remaining has not been implemented yet.", this);
+                                break;
+                            }
+                    }
+
+                    ///Parse all the info of the activity in the activity array and actually turn it into...
+                    ///- drum roll -
+                    ///A discord activity.
+                    activity = new Discord.Activity()
+                    {
+                        State = activityobject.state,
+                        Details = activityobject.details,
+                        Timestamps =
+                    {
+                        Start = timestamp
+                    },
+                        Assets =
+                    {
+                        LargeImage = activityobject.bigImageKey,
+                        SmallImage = activityobject.smallImageKey,
+                        LargeText = activityobject.bigImageKeyText,
+                        SmallText = activityobject.smallImageKeyText
+                    }
+
+                    };
+                    ///Update the activity
+                    activityManager.UpdateActivity(activity, (res) =>
+                    {
+                        if (res == Discord.Result.Ok)
+                        {
+                            Debug.Log("Status Update Success! [Discord]");
+                        }
+                        else
+                        {
+                            Debug.Log("Status Update Failed! [Discord]");
+                        }
+                    });
+                    return;
+
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            if (count == 0)
+            {
+                activity = new Discord.Activity()
+                {
+                    State = "No state was found for this scene."
+                };
+                activityManager.UpdateActivity(activity, (res) =>
+                {
+                    if (res == Discord.Result.Ok)
+                    {
+                        Debug.Log("Status Update Success! [Discord]");
+                    }
+                    else
+                    {
+                        Debug.Log("Status Update Failed! [Discord]");
+                    }
+                });
+            }
+
+        }
+
+
+
+
+    }
+    [System.Serializable]
+    public class DiscordActivityObject
+    {
+        public string scene;
+        public string details, state;
+        public string bigImageKey, smallImageKey, bigImageKeyText, smallImageKeyText;
+        public TimeStampMode timestampMode;
+        public enum TimeStampMode
+        {
+            None,
+            Elapsed,
+            Remaining
+        }
+    }
+}
---
+++ Runtime/Spettro/Health/Health.cs
@@ -0,0 +1,119 @@
+using Spettro.HealthSystem;
+using UnityEngine;
+using UnityEngine.Events;
+
+#if UNITY_EDITOR
+namespace Spettro.Editors
+{
+    [UnityEditor.CustomEditor(typeof(Health))]
+    public class HealthEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            Health h = (Health)target;
+
+            if (UnityEditor.EditorApplication.isPlaying)
+            {
+
+                if (GUILayout.Button("Damage"))
+                {
+                    h.Damage(1);
+                }
+                if (GUILayout.Button("Heal"))
+                {
+                    h.Heal(1);
+                }
+                if (GUILayout.Button("Kill"))
+                {
+                    h.Damage(h.maxHP);
+                }
+            }
+            else
+                UnityEditor.EditorGUILayout.HelpBox("You can control the health script from the inspector if you're in Play Mode.", UnityEditor.MessageType.Info);
+        }
+    }
+}
+#endif
+namespace Spettro.HealthSystem
+{
+    public class Health : MonoBehaviour
+    {
+        public int HP;
+        public int maxHP;
+        public int livesCount;
+
+        [Header("Options")]
+        public bool log = true;
+        public bool lives = false;
+        public bool resetOnDeath = false;
+
+        [Header("Events")]
+        public UnityEvent<int> OnDamage;
+        public UnityEvent<int> OnHeal;
+        public UnityEvent<int> OnDeath;
+        public UnityEvent OnLivesOver;
+        public void Start()
+        {
+            HealthSet();
+        }
+        public void HealthSet()
+        {
+            HP = maxHP;
+        }
+
+        public void Damage(int amount)
+        {
+            Debug.Log("Recieved damage of " + amount);
+            HP -= amount;
+            if (HP <= 0)
+            {
+                HP = 0;
+                if (OnDeath != null)
+                {
+                    OnDeath.Invoke(HP);
+                    if (log)
+                        Debug.LogWarning(gameObject.name + " has died.");
+                    if (resetOnDeath)
+                        HealthSet();
+                    if (lives)
+                    {
+                        livesCount--;
+                        if (livesCount < 0)
+                            OnLivesOver.Invoke();
+                    }
+
+                }
+
+            }
+            if (OnDamage != null)
+            {
+                OnDamage.Invoke(HP);
+                if (log)
+                    Debug.LogWarning($"{gameObject.name} recieved damage ({amount}). New HP: {HP}");
+            }
+
+        }
+        public void Heal(int amount)
+        {
+            HP += amount;
+            if (HP > maxHP)
+            {
+                HP = maxHP;
+            }
+            if (OnHeal != null)
+            {
+                OnHeal.Invoke(HP);
+                if (log)
+                    Debug.LogWarning($"{gameObject.name} has been healed by {amount}. New HP: {HP}");
+            }
+        }
+
+        public float GetHPNormalized()
+        {
+            return (float)HP / maxHP;
+        }
+
+    }
+}
