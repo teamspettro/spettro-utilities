fb6f557745d15da0c07748a966c242121eb2c458
NextinMono <lorenzorai05@gmail.com>
2022-01-16 10:58:23 +0100

    fixed

--- Runtime/Spettro/Health/Health.cs
+++ Runtime/Spettro/Health/Health.cs
@@ -1,42 +1,7 @@
-using Spettro.HealthSystem;
 using UnityEngine;
 using UnityEngine.Events;
 
-#if UNITY_EDITOR
-namespace Spettro.Editors
-{
-    [UnityEditor.CustomEditor(typeof(Health))]
-    public class HealthEditor : UnityEditor.Editor
-    {
-        public override void OnInspectorGUI()
-        {
-            DrawDefaultInspector();
-
-            Health h = (Health)target;
-
-            if (UnityEditor.EditorApplication.isPlaying)
-            {
-
-                if (GUILayout.Button("Damage"))
-                {
-                    h.Damage(1);
-                }
-                if (GUILayout.Button("Heal"))
-                {
-                    h.Heal(1);
-                }
-                if (GUILayout.Button("Kill"))
-                {
-                    h.Damage(h.maxHP);
-                }
-            }
-            else
-                UnityEditor.EditorGUILayout.HelpBox("You can control the health script from the inspector if you're in Play Mode.", UnityEditor.MessageType.Info);
-        }
-    }
-}
-#endif
-namespace Spettro.HealthSystem
+namespace Spettro.HP
 {
     public class Health : MonoBehaviour
     {
